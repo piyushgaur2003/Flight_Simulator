4155d7c7dfdbd70da5def14460c3149239d244d8
piyushgaur2003 <piyushgaur1001@gmail.com>
2025-03-16 17:09:47 +0530

    Next: Drag

---
+++ Assets/Script/AircraftController.cs
@@ -0,0 +1,62 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class AircraftController : MonoBehaviour
+{
+    [SerializeField] private Transform CM;
+    [SerializeField] private Transform liftPoint;
+    [SerializeField] private Transform enginePoint;
+
+    [SerializeField] private Rigidbody rb;
+
+    [SerializeField] private float airPressure = 14.7f;
+    [SerializeField] private float wingArea = 10f;
+    [SerializeField] private float verticalStabilizerArea = 1f;
+
+    [SerializeField] private AnimationCurve c1;
+    [SerializeField] private AnimationCurve rudderAoa;
+
+    public float Aoa;
+    public float AoaYaw;
+    public float enginePower;
+
+    public Vector3 velocity;
+    public Vector3 localVelocity;
+    public Vector3 localAngularVelocity;
+
+    void Start()
+    {
+        rb.centerOfMass = CM.localPosition;
+    }
+
+    void FixedUpdate() {
+        CalculateState();
+
+        c1 = AnimationCurve.Linear(-10, 0, 10, 1);
+        rb.AddForceAtPosition(-enginePoint.forward * enginePower, enginePoint.position);
+        float lift = c1.Evaluate(Aoa) * ((airPressure * (localVelocity.z * localVelocity.z)) / 2) * wingArea;
+        lift = Mathf.Max(lift, 0.1f);
+        rb.AddForceAtPosition(liftPoint.up * lift, liftPoint.position);
+
+        rb.AddTorque(transform.right * lift * 0.01f);
+        rb.AddTorque(transform.up * rudderAoa.Evaluate(AoaYaw) * (0.5f * airPressure * (localVelocity.z * localVelocity.z)) * verticalStabilizerArea);
+
+        Debug.Log($"Lift Force: {lift}");
+        Debug.Log($"Velocity: {velocity}");
+        Debug.Log($"Angle of Attack: {Aoa}");
+    }
+
+    void CalculateState(){
+        var invRot = Quaternion.Inverse(rb.rotation);
+        velocity = rb.velocity;
+        localVelocity = invRot * velocity;
+        localAngularVelocity = invRot * rb.angularVelocity;
+
+        CalculateAngleOfAttack();
+    }
+
+    void CalculateAngleOfAttack(){
+        Aoa = Mathf.Atan2(-localVelocity.y, localVelocity.z) * 57.2957795f;
+        AoaYaw = Mathf.Atan2(localVelocity.x, localVelocity.z) * 57.2957795f;
+    }
+}
